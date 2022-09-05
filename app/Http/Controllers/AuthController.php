e77dad86c708196f74dbeb1e1ba9b498e3811eba
adrianhAD <adrian@amazingdiscoveries.org>
2022-09-05 16:36:27 -0700

    Jwt authentication implemented

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+use Validator;
+
+class AuthController extends Controller
+{
+    public function __construct(){
+        $this->middleware('auth:api', ['except'=>['login','register']]);
+    }
+
+    public function register(Request $request){
+        $validator = Validator::make($request->all(),[
+            'name' => 'required',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed|min:6',
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+        $user = User::create(array_merge(
+            $validator->validated(),
+            ['password' => bcrypt($request->password)]
+        ));
+
+        if($user){
+            return response()->json([
+                'message' => 'We are happy to register your account.',
+                'user' => $user
+            ], 200);
+        }else{
+            return response()->json([
+                'message' => 'Sorry, something went wrong, it was not possible register your account.',
+            ], 200);
+        }
+    }
+
+    public function login(Request $request){
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        if(! $token =auth('api')->attempt($validator->validate())){
+            return response()->json(['error'=>'Invalid credentials.'], 422);
+        }
+
+        return $this->createNewToken($token);
+    }
+
+    public function createNewToken($token){
+        return response()->json([
+            'token'  => $token,
+            'token_type'    => 'bearer',
+            'expires_in'    => auth('api')->factory()->getTTL() * 60,
+            'name'          => auth('api')->user()->name,
+        ]);
+    }
+}
